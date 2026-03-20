0b0f0f6852f809429be385c7d25d5c9305ac3ed7
Santos, Thiago R <thiago_thg@outlook.com>
2026-03-20 11:48:33 -0300

    fix: Removed trash from old version

--- lib/app/core/enums/app_enums.dart
+++
@@ -1,7 +0,0 @@
-enum ResponseStatus {rsSuccess, rsFalied}
-
-enum AuthStatus { loading, login, logoff }
-// ignore: constant_identifier_names
-enum AuthFirebaseError { UserNotFound, PasswordNotValid, NetworkError }
-// ignore: constant_identifier_names
-enum LoginState {IDLE, LOADING, SUCCESS, FAIL}
